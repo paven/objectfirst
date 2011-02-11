6390fd466a5e071f2bd49d302875c3cf5dc5a0ed
thepaven <thepaven@375464ce-beed-0cfd-58c8-3ef171364725>
2011-02-11 09:19:02 +0000

    sv fail fix

--- trunk/lit310/src/objectsfirst/controller/Collideble.java
+++ trunk/lit310/src/objectsfirst/controller/Collideble.java
@@ -6,13 +6,21 @@
 package objectsfirst.controller;
 
 import java.awt.Point;
+import java.awt.Rectangle;
 
 /**
  *
  * @author Gustafsp
  */
 public interface Collideble extends Positionable{
-    void collidesWith(Collideble other);
-    void collidedWith(Collideble other);
-    boolean checkPixel(Point point);
+    /**
+     
+     * @return the area the objects ocupies.
+     */
+    public Rectangle getCollisionBox();
+    public boolean collidesWith(Collideble other);
+    public boolean collidesWith(Rectangle other);
+    public boolean collidesWith(Point point);
+    public void collidedWith(Collideble other);
+    
 }
