1aef46a5c5823c04c05e637a5172b3ee4a54e4d4
thepaven <thepaven@375464ce-beed-0cfd-58c8-3ef171364725>
2011-02-15 10:54:16 +0000

    DONE: compleated v0.1 ecported project to eclipse to create jar. TODO get the damn jar to work

--- lit310/src/nu/mumma/lit310/recycleGame/sprites/Trash.java
+++ lit310/src/nu/mumma/lit310/recycleGame/sprites/Trash.java
@@ -4,14 +4,14 @@
  */
 package nu.mumma.lit310.recycleGame.sprites;
 
-import static nu.mumma.lit310.objectsfirst.start.Start.remove;
+import static nu.mumma.lit310.objectsFirst.start.Start.remove;
 import java.io.IOException;
 import java.util.HashMap;
 import nu.mumma.lit310.objectsFirst.core.abstraction.Collideble;
 import nu.mumma.lit310.objectsFirst.core.abstraction.Paintable;
 import nu.mumma.lit310.objectsFirst.core.abstraction.Solid;
 import nu.mumma.lit310.objectsFirst.recycleGame.abstraction.Recyclable;
-import nu.mumma.lit310.objectsfirst.start.Start;
+import nu.mumma.lit310.objectsFirst.start.Start;
 
 /**
  *
